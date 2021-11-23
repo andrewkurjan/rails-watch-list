a55deb8ae957666efa4025a3bd55c022af752ca3
Andrew Kurjan <andrewkurjan@gmail.com>
2021-11-23 21:09:15 +1100

    finished app logic, completed bookmarks controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,11 +13,20 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
 
-    redirect_to list_path(@list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to root_path
+  end
 
 
   private
